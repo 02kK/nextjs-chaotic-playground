1c87cd34bcd09c92214a3c7a7399e80dad2f7b2a
02kK <lahass1213@gmail.com>
2022-07-16 16:36:46 +0900

    JSXでコンパイルエラーが出た際の対処法コメント追記

--- src/pages/playground/markup.tsx
+++ src/pages/playground/markup.tsx
@@ -1,6 +1,16 @@
 import { useEffect } from "react";
+import { ReactDOM } from "react";
 
 import styles from "../../../styles/markup.module.css";
+
+// Property does not exist on type 'JSX.IntrinsicElements'のコンパイルエラーとなった際の妥協策
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      rb: any;
+    }
+  }
+}
 
 const Markup = () => {
   // JSXの場合にはscript要素を使うのではなく副作用はuseEffectで記述
@@ -35,6 +45,18 @@ const Markup = () => {
         </p>
       </a>
       <hr />
+      <p>
+        {/* ルビのつけかた */}
+        <ruby>
+          <rb>局地</rb>
+          <rt>きょくち</rt>
+          <rb>気象</rb>
+          <rt>きしょう</rt>
+          <rb>予報</rb>
+          <rt>よほう</rt>
+        </ruby>
+      </p>
+
       <p>&copy; &lt;Playground Markup Sample&gt;</p>
     </main>
   );

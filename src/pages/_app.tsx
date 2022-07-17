bcfa4ea9681b68d5e7d64a0e6bc47c7863d16ebf
02kK <lahass1213@gmail.com>
2022-07-18 00:30:20 +0900

    sass化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/app";
 
-import "../../styles/globals.css";
+import "../../styles/globals.sass";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

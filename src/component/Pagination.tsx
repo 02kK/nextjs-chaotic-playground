fce9cff7ee679c7240f4cc99609f93d0bdde1dd1
queuek <lahass1213@gmail.com>
2022-07-31 23:02:49 +0900

    Pagination実装

--- src/component/Pagination.tsx
+++ src/component/Pagination.tsx
@@ -1,6 +1,7 @@
 //components/Pagination.js
 import Router from "next/router";
 import Link from "next/link";
+import styled from "@emotion/styled";
 
 type PaginationProps = { totalCount: number; perPage: number };
 
@@ -11,14 +12,26 @@ export const Pagination = (props: PaginationProps) => {
     [...Array(end - start + 1)].map((_, i) => start + i);
 
   return (
-    <ul>
+    <SPagination>
       {range(1, Math.ceil(totalCount / perPage)).map((number, index) => (
         <li key={index}>
-          <Link href={`/article/paginate/${number}`}>
+          <Link href={`/article/pagination/${number}`}>
             <a>{number}</a>
           </Link>
         </li>
       ))}
-    </ul>
+    </SPagination>
   );
 };
+
+const SPagination = styled.ul`
+  display: flex;
+  li {
+    margin-right: 20px;
+    padding: 2px 5px;
+    border: 1px solid #777;
+    list-style: none;
+    background-color: #555;
+    color: #fff;
+  }
+`;
--- src/pages/article/pagination/[id].tsx
+++ src/pages/article/pagination/[id].tsx
@@ -1,3 +1,46 @@
-const Page = () => {};
+import { GetStaticPaths, NextPage } from "next";
+
+import { Pagination } from "../../../component/Pagination";
+import { ArticleCard } from "../../../component/ArticleCard";
+import mockData from "../../../mock/mockData";
+
+type PageProps = { id: string };
+
+const Page: NextPage<PageProps> = (props) => {
+  const { id } = props;
+  const targetMockData = mockData.slice(5 * (Number(id) - 1), 5 * Number(id));
+  return (
+    <div style={{ margin: "10px" }}>
+      {targetMockData.map((id) => (
+        <ArticleCard
+          key={id}
+          id={id}
+          thumbnailImg="/img/shared/my_icon.jpeg"
+          articleTitle="This is Article Title"
+          summaryText={String.fromCodePoint(Number(id) + 12400).repeat(500)}
+        />
+      ))}
+      <Pagination totalCount={mockData.length} perPage={5} />
+    </div>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = [
+    { params: { id: "1" } },
+    { params: { id: "2" } },
+    { params: { id: "3" } },
+  ];
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async (context: any) => {
+  const id = Array.isArray(context.params["id"])
+    ? context.params["id"][0]
+    : context.params["id"];
+  return {
+    props: { id },
+  };
+};
 
 export default Page;
